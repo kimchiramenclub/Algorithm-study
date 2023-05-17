71e15a30fd113f5984419448e08af14e2532748b
joon <june.w.lim@gmail.com>
2023-05-17 14:57:26 +0900

    [자료구조]: Dynamic Array Test를 JUnit 테스트 파일로 변경

--- src/자료구조/DynamicArr/DynamicArrTest.java
+++ src/자료구조/DynamicArr/DynamicArrTest.java
@@ -1,32 +1,34 @@
 package 자료구조.DynamicArr;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DynamicArrTest {
-    public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
+
+    @Test
+    public void testDynamicArrayMethods() {
         DynamicArr<Integer> dynamicArray = new DynamicArr<>(2);
-        sb.append("동적 배열 capacity 설정 : ").append(dynamicArray.capacity()).append("\n");
+
+        // 동적 배열 capacity 설정 체크
+        assertEquals(2,dynamicArray.capacity());
+
+        // 동적 배열에 item 추가
         dynamicArray.add(1);
         dynamicArray.add(2);
         dynamicArray.add(3);
-        sb.append("\n동적 배열에 1,2,3 추가\n").append("\n");
 
-        sb.append("동적 배열 capacity: ").append(dynamicArray.capacity()).append("\n");
-        sb.append("동적 배열 size: ").append(dynamicArray.size()).append("\n");
+        // 동적 배열 capacity resizing(doubling) 체크
+        assertEquals(4, dynamicArray.capacity());
+        assertEquals(3, dynamicArray.size());
+        assertEquals(Integer.valueOf(1), dynamicArray.get(0));
+        assertEquals(Integer.valueOf(2), dynamicArray.get(1));
+        assertEquals(Integer.valueOf(3), dynamicArray.get(2));
 
-        sb.append("동적 배열 : [");
-        for(int i=0;i<dynamicArray.size();i++) sb.append(dynamicArray.get(i)).append(" ");
-        sb.setLength(sb.length()-1);
-        sb.append("]").append("\n");
-
+        // index 1의 값 제거후 동적 배열 size 체크
         dynamicArray.remove(1);
-        sb.append("\nIndex 1 제거\n\n");
-
-        sb.append("변경된 동적 배열 size: ").append(dynamicArray.size()).append("\n");
-        sb.append("변경된 동적 배열 : [");
-        for(int i=0;i<dynamicArray.size();i++) sb.append(dynamicArray.get(i)).append(" ");
-        sb.setLength(sb.length()-1);
-        sb.append("]").append("\n");
-
-        System.out.println(sb);
+        assertEquals(2, dynamicArray.size());
+        assertEquals(Integer.valueOf(1), dynamicArray.get(0));
+        assertEquals(Integer.valueOf(3), dynamicArray.get(1));
     }
 }
